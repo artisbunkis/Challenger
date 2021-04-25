33838d47a30f4836c3a07bf8c3f67056e67c558c
RichardsZv <richards.zvagulis@gmail.com>
2021-04-25 14:01:01 +0300

    Challenge create forma v1.0. Pievienoju ChallengeController, challenge model un challenge create formu.

---
+++ app/Models/Challenge.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+//
+use App\Models\User;
+
+class Challenge extends Model
+{
+    use HasFactory;
+
+    
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<!DOCTYPE html>    
+<html>  
+    <head>    
+        <title>Home</title>         
+    </head> 
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create challenge</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="form">
+                        {{--aizkomenteto atkomentet kad izveidots lidz galam kontroleris un parejais--}}
 
-                    {{ __('You are logged in!') }}
-                    {{ __('Welcome, ') }}<b>{{ Auth::user()->username }}</b>
+                        
                     
+                        <form method="POST" > @csrf
+                        {{-- <input type="hidden" name="challenge_ID" value="{{ $challenge->id }}"> 
+                        <input type="hidden" name="sportsType_ID" value="{{ $challenge->foreignId }}"> 
+                        <input type="hidden" name="creatorUser_ID" value="{{ $challenge->foreignId }}">  --}}
+                        
+                        <label for="challengeName">Chellenge Name: </label>
+                        <input type="text" name="challengeName" id="challengeName"><br>
+
+                        <label for="beginDate">Begin Date:</label>
+                        <input type="date" name="beginDate" id="beginDate">
+
+                        <label for="endDate">End Date:</label>
+                        <input type="date" name="endDate" id="endDate"><br>
+
+                        <label for="isPublic">Public: </label>
+                        <input type="checkbox" checked name="isPublic" id="isPublic"><br>
+
+                        <input type="submit" value="add">
+                        </form>
                     
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+</html>
 @endsection
