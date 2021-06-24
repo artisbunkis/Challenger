0ebb13a32e0a8be0970796af1fa12d7007ca9205
RichardsZv <richards.zvagulis@gmail.com>
2021-06-24 12:04:31 +0300

    Register redirect fix Now redirects to user profile when registered

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const PROFILE = '/profile';
 
     /**
      * The controller namespace for the application.
