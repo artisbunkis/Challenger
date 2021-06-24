a6f1b1568ee2e5608e3ae3cb6212dce236b8f86f
RichardsZv <richards.zvagulis@gmail.com>
2021-06-24 13:04:44 +0300

    Login route fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::PROFILE;
 
     /**
      * Create a new controller instance.
