58089186d8b3f32dd06c464b139f0d0d4a47d0a3
RichardsZv <richards.zvagulis@gmail.com>
2021-06-27 19:21:08 +0300

    Find challenges filtrs (JS)

--- resources/views/findchallenges.blade.php
+++ resources/views/findchallenges.blade.php
@@ -3,37 +3,191 @@
 <!DOCTYPE html>    
 <html>  
     <head>    
-        <title>Find Challenge</title>         
+        <title>Find Challenge</title>   
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/mystyle.css') }} ">     
     </head> 
+
 <body>
 
 
-{{--
-@isset($challenges)
-@foreach($challenges as $challenge)
-    <p>{{$challenge}}</p>
-    
-@endforeach
-@endisset
---}}
+
 <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
 <div class="container">
-    <div class="d-flex justify-content-between" id="myBtnContainer">
+    <div id="myBtnContainer" class="d-flex justify-content-between">
+        
         <div >
-            <button type="button" class="btn btn-secondary btn-sm butn" style="font-size: 14px;" onclick="filterSelection(1000)" value="1000">All</button></p>   
+            <button class="btn" onclick="filterSelection('all')">All</button>   
         </div>
+        
         @isset($sportsTypes)
             @foreach($sportsTypes as $sportsType)
-            <div >
-                <button type="button" class="btn btn-secondary btn-sm butn" style="font-size: 14px;" onclick="filterSelection($sportsType->sportsTypeID)" value="{{$sportsType->sportsType_ID}}">{{$sportsType->sportsTypeName}}</button></p>   
+            <div>
+                <button class="btn" onclick="filterSelection('{{$sportsType->sportsTypeName}}')">{{$sportsType->sportsTypeName}}</button>  
             </div>
                 
             @endforeach
         @endisset
-      </div>
-    
-    <div class="row" data-masonry='{"percentPosition": true }'>
+    </div>
+
+    <div class="container">
         
+        
+      
+        @isset($challenges)
+        @foreach($challenges as $challenge)
+            @isset($sportsTypes)
+                @foreach($sportsTypes as $sportsType)
+                    @if($sportsType->sportsType_ID == $challenge->sportsType_ID)  
+                                                
+                        
+                        <div class="card mb-3 filterDiv {{$sportsType->sportsTypeName}}">
+                        <div class="card-header text-center "><h2>{{$challenge->challengeName}}</h2></div>
+                            <div class="card-body text-md-left">
+                                @isset($sportsTypes)
+                                @foreach($sportsTypes as $sportsType)
+                                @if($sportsType->sportsType_ID == $challenge->sportsType_ID)                                
+                                    <div  id="sportsBox">
+                                        <p><b>Sports Type:</b> {{$sportsType->sportsTypeName}}</p>   
+                                    </div>
+                                @endif
+                                @endforeach
+                                @endisset
+    
+                                <p><b>Subscriber Count: </b>
+                                    @isset($subscribedCountArray)
+                                        @foreach ($subscribedCountArray as $s)
+                                            @if($s[0] == $challenge->challenge_ID)
+                                                {{$s[1]}}
+                                            @endif
+                                        @endforeach
+                                    @endisset
+                                </p>
+    
+                                @isset($measurements)
+                                @foreach($measurements as $measurement)
+                                @if($challenge->challenge_ID == $measurement->challenge_ID)
+                                    <p><b>Goal:</b> 
+                                        @foreach ($units as $unit)
+                                            @if($unit->unit_ID == $measurement->unit_ID)
+                                                @foreach ($comparisons as $comparison)
+                                                    @if ($measurement->comparison_ID == $comparison->comparison_ID)
+                                                        {{ $unit->unitName }} {{ $comparison->comparisonSign }} {{$measurement->goalValue}} {{ $unit->unitCode }}
+                                                    @endif
+                                                @endforeach
+                                                
+                                            @endif
+                                        @endforeach
+                                        </p>
+                                @endif
+                                @endforeach
+                                @endisset
+    
+                                
+                                <p><b>Ends in:</b> {{$challenge->endDate}}</p>
+    
+                                @isset($user_ids)
+                                @foreach($user_ids as $user_id)
+                                @if($user_id->user_ID == $challenge->creatorUser_ID)
+                                    <p><b>Creator:</b> {{$user_id->username}}</p>
+                                @endif
+                                @endforeach
+                                @endisset
+                                
+                                <?php $irSaraksta = false ?>
+                                
+                                @isset($subscrChal)
+                                    @foreach ($subscrChal as $sc)
+                                        @if($sc->challenge_ID == $challenge->challenge_ID)
+                                            @php $irSaraksta = true @endphp
+                                            @break
+                                        @endif
+                                    
+                                    @endforeach
+                                    @isset($irSaraksta)
+                                        @if ($irSaraksta)
+                                        <form method="POST" id="forma" action="{{ action([App\Http\Controllers\FindChallengesController::class, 'unsubscribe'], $challenge->challenge_ID)}}"> @csrf @method('POST')
+                                                        
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                            <input type="hidden" name="id" value="{{ $challenge->challenge_ID }}">
+                                            
+                                        
+                                             <button type="submit">Unsubscribe</button>
+                                        </form>
+                                        @else
+                                        <form method="POST" id="forma" action="{{ action([App\Http\Controllers\FindChallengesController::class, 'subscribe'], $challenge->challenge_ID)}}"> @csrf @method('POST')
+                                                        
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                            <input type="hidden" name="id" value="{{ $challenge->challenge_ID }}">
+                                            
+                                        
+                                             <button type="submit">Subscribe</button>
+                                        </form>
+                                        @endif
+                                    @endisset
+                                @endisset
+                                
+                            </div>
+                        
+                    </div>
+    
+                    
+                    @endif
+                @endforeach
+             @endisset
+        @endforeach
+        @endisset
+        
+    </div>
+
+    
+<script>
+    filterSelection("all")
+    function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+    }
+
+    function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+    }
+
+    function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);     
+        }
+    }
+    element.className = arr1.join(" ");
+    }
+
+    // Add active class to the current button (highlight it)
+    var btnContainer = document.getElementById("myBtnContainer");
+    var btns = btnContainer.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+    }
+
+</script>
+
+    <div class="row" data-masonry='{"percentPosition": true }'>
+        {{--
         @isset($challenges)
         @foreach($challenges as $challenge)
         <div class="col-md-4 @isset($sportsTypes)@foreach($sportsTypes as $sportsType)@if($sportsType->sportsType_ID==$challenge->sportsType_ID){{$sportsType->sportsType_ID}}@endif @endforeach @endisset">
@@ -124,28 +278,16 @@
                                         @endif
                                     @endisset
                                 @endisset
-    
-                                {{--
-                                    <form method="POST" id="forma" action="{{ action([App\Http\Controllers\FindChallengesController::class, 'subscribe'], $challenge->challenge_ID)}}"> @csrf @method('POST')
-                                                        
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <input type="hidden" name="id" value="{{ $challenge->challenge_ID }}">
-                                            
-                                        
-                                             <button type="submit">Subscribe</button>
-                                        </form>
-                                    --}}
                                 
-                            
                             </div>
-    
+                        
                     </div>   
                 </div>
             </div>
-            
         @endforeach
         @endisset
     </div>
+   --}}
 </div>
 
 @can('is-admin')
@@ -188,7 +330,7 @@
                         @endisset
                         </table>
                     </div>
-
+                </div>
             </div>   
         </div>
 
@@ -199,52 +341,6 @@
 
 
 </body>
-
-<script>
-    filterSelection(1000)
-    function filterSelection(c) {
-        var x, i;
-        x = document.getElementsByClassName("col-md-4");
-        if (c == 1000) c = "";
-        for (i = 0; i < x.length; i++) {
-            w3RemoveClass(x[i], "show");
-            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-        }
-    }
-
-    function w3AddClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-            if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-        }
-    }
-
-    function w3RemoveClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split("        ");
-        arr2 = name.split("        ");
-        for (i = 0; i < arr2.length; i++) {
-            while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);     
-            }
-        }
-        element.className = arr1.join("        ");
-    }
-
-    // Add active class to the current button (highlight it)
-    var btnContainer = document.getElementById("myBtnContainer");
-    var btns = btnContainer.getElementsByClassName("btn");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function(){
-            var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        });
-    }
-
-</script>
 
 </html>
 @endsection
