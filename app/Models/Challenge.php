1dfa08cfc96d35fa1e68f3a6497de018fc43b058
Artis <62614471+artisbunkis@users.noreply.github.com>
2021-06-29 23:28:32 +0300

    FK Keys

--- app/Models/Challenge.php
+++ app/Models/Challenge.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use App\Models\User;
-use App\Models\Activity;
+use App\Models\Challenge_Measurements;
+use App\Models\SportsType;
+use App\Models\Subscription;
+
 
 class Challenge extends Model
 {
@@ -14,10 +16,17 @@ class Challenge extends Model
 
     use HasFactory;
 
-    public function activities(){//FK
-        return $this->hasMany(Activity::class);//Challenge has many activities
-    }
+    public function challenge_measurements(){//FK
+        return $this->hasMany(Challenge_Measurements::class);//Challenge has many activities
+    } 
     
+    public function subscriptions(){//FK
+        return $this->hasMany(Subscription::class);//Challenge has many activities
+    } 
+    
+    public function sportsTypes(){//FK
+        return $this->belongsTo(SportsType::class);//Challenge has many activities
+    } 
     // public function users(){//FK
     //     return $this->morphToMany(User::class);//Challenge has users, user can have multiple challenges
     // }
--- app/Models/Gender.php
+++ app/Models/Gender.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Gender extends Model
 {
     protected $table = "gender";
     use HasFactory;
+    
+    public function users(){//FK
+        return $this->hasMany(User::class);
+    }
 }
