7ec76323008ce7673a99062ee1061f48d82e92b3
RichardsZv <richards.zvagulis@gmail.com>
2021-06-29 15:52:04 +0300

    Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,12 @@
 <html>    
     <head>    
         <title>Login Form</title>   
-        <link rel="stylesheet" type="text/css" href="css/style.css"> 
-        <link href="{{ asset('/css/style.css') }}" rel="stylesheet">    
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/welcomestyle.css') }} ">   
     </head>    
     <body>
-        <div class="d-flex align-items-center justify-content-center" style="height: 350px">
-            <h1>Welcome To Challenger</h1>
-            <h1></h1>
+
+        <div class="d-flex align-items-center justify-content-center" style="height: 90vh">
+            <h1 class="welcomeText fade-in">Welcome To Challenger!</h1>
         </div>
         
          
