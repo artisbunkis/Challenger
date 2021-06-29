2257b9e1faada77406c3d1a0106d7ee141ed0901
Artis <62614471+artisbunkis@users.noreply.github.com>
2021-06-29 22:10:24 +0300

    Delete subscriptions as admin

--- app/Http/Controllers/FindChallengesController.php
+++ app/Http/Controllers/FindChallengesController.php
@@ -95,7 +95,7 @@ class FindChallengesController extends Controller
      */
     public function erase(Request $request)
     {   
-        
+        $subscriptions = Subscription::all()->where('challenge_ID', '=', $request->id)->delete();
         $challenge = Challenge::where('challenge_ID', '=', $request->id)->delete();        
         return redirect('findchallenges');
     }
