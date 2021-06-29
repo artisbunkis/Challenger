120433d24de62ac2da9ab6cba517810f5929e782
RichardsZv <richards.zvagulis@gmail.com>
2021-06-29 16:26:06 +0300

    background blur

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,28 +4,26 @@
 <!DOCTYPE html>    
 <html>    
     <head>    
-
         <title>Login Form</title>   
         <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/welcomestyle.css') }} ">   
     </head>    
     <body>
-
-        
-
         <title>{{ __("Login Form")}}</title>   
         <link rel="stylesheet" type="text/css" href="css/style.css"> 
         <link href="{{ asset('/css/style.css') }}" rel="stylesheet">    
     </head>    
     <body>
-        <div class="d-flex align-items-center justify-content-center" style="height: 90vh">
+        <div class="bg-image"></div>
+        <div class="d-flex align-items-center justify-content-center" style="height: 100vh">
             <h1 class="welcomeText fade-in">{{ __("Welcome To Challenger")}}!</h1>
             <h1></h1>
 
         </div>
         
+    
          
             
-    </div>    
+       
     </body>    
 </html>     
 @endsection
